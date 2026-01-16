40c2c40d726170a314b3d9160d28e3123a9462e8
JosipRoncevic <josiproncevic8@gmail.com>
2026-01-16 13:12:07 +0100

    Firebase setup

---
+++ src/ui/world/WorldsScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const WorldsScreen =() =>{
+    return (
+        <View style={{justifyContent:"center",alignItems:'center',flex:1}}>
+            <Text>Worlds aaaaa</Text>
+        </View>
+    )
+}
+
+export default WorldsScreen
+
+const styles = StyleSheet.create({})
